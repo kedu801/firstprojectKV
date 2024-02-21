89120903110f78e82972653ee4861be5cba20877
kedu801 <160650296+kedu801@users.noreply.github.com>
2024-02-20 22:36:16 -0300

    scriptcommit

--- script.js
+++ script.js
@@ -4,11 +4,4 @@ function toggleMode() {
 
   const image = document.querySelector("#profile img")
 
-  if (html.classList.contains("light")) {
-
-    image.setAttribute("src", "./assets/avatar.lightmeu.png")
-    
-  } else {
-    image.setAttribute("src", "./assets/aavatar.png")
-  }
 }
